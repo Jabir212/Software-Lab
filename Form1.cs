496d34abffa8cf2d56b43af712cd39605c43a2bc
Jabir Ahmed <152061052+Jabir212@users.noreply.github.com>
2025-11-09 20:19:51 +0600

    Add comment indicating editing location
    edited at h

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//edited at home
 namespace Hostel_Managment_System
 {
     public partial class Form1 : Form
